8f483a0b294b04a5deaabf6079d8d3743da9a983
tkimu3 <t.kimu3@gmail.com>
2023-06-27 08:05:21 +0900

    finish base_model_comparison

--- base_models.py
+++ base_models.py
@@ -36,142 +36,193 @@ from sklearn.preprocessing import StandardScaler
 # import warnings filter
 from warnings import simplefilter
 
+from pathlib import Path, PurePath
+
 def main():
 
     # ignore all warnings
     simplefilter(action='ignore', category=Warning)
 
     ## Read the csv files
+    # get current relative path
+    cur_dir = Path('.')
 
-    # Import features from a CSV file
-    df_features = pd.read_csv("1677CVEs_783dim_features_20230326.csv")
-    df_features.rename({"Unnamed: 0" : "CVE"}, inplace = True, axis = 1)
-    df_features.set_index('CVE', inplace = True)
+    # get current absolute path
+    # cur_dir = Path.cwd()
 
-    # Filter the features which are NaN# %%
-    impute(df_features)
-    # y = df_flag_sorted['flag_H']
-    # features_filterd = select_features(df_features, y)
-    # features_filterd
+    # feeatures directory
+    data_dir = 'features'
+
+    # join the directory
+    parent_dir = cur_dir.joinpath(data_dir)
+
+    # Define file name to read
+    # file_name = '01_tweet_count.csv'
+
+    # # wild card search for csv file paths to read
+    filePaths = sorted(list(parent_dir.rglob("*.csv")))
+    # print(filePaths[0]) -> features/06_impression_count.csv
+    # print(filePaths[0].resolve()) -> /home/username/detect_vul_with_tw_trend/features/06_impression_count.csv
+    # print(filePaths[0].with_suffix('').as_posix()) -> features/06_impression_count
+    # print(filePaths[0].name) -> 06_impression_count.csv
+    # print(filePaths[0].stem) -> 06_impression_count
 
     #  Import the CVE list with flags from the CSV file
     df_flag = pd.read_csv("CVE_list_with_flagH_20230326.csv", index_col = ['CVE'] )
     df_flag_sorted = df_flag.sort_values('CVE')
     df_flag_sorted.drop('Unnamed: 0', inplace = True, axis = 1)
+    df = []
+
+    for i, filepath in enumerate(filePaths):
+        # extract filename from filepath
+        file_name = filepath.name
+        print('\n'+ file_name)
+
+        # join paths to the file
+        file_path = parent_dir.joinpath(file_name)
+        # print(file_name) -> 06_impression_count.csv
+        # print(file_path) -> features/06_impression_count.csv
+
+        # Import features from a CSV file
+        df.append(pd.read_csv(file_path))
+        df[i].rename({"Unnamed: 0" : "CVE"}, inplace = True, axis = 1)
+        df[i].set_index('CVE', inplace = True)
+
+        # Filter the features which are NaN# %%
+        impute(df[i])
+        # print(df[i].head())
+
+        # y = df_flag_sorted['flag_H']
+        # features_filterd = select_features(df[i], y)
+        # features_filterd
+
+        # if the numbers of features are less than CVEs with flag, drop the CVEs that is not in the features
+        if (file_name == '02_retweet_count.csv'):
+            df_flag_temp = df_flag_sorted
+            df_flag_sorted = df_flag_sorted[df_flag_sorted.index.isin(df[i].index)]
+
+        ## Create a Validation dataset
+        # Define parameters for each model
+        validation_size = 0.30
+        seed = 1234
+        num_splits = 10
+        scorings = ['accuracy','precision','recall','f1']
+
+        # split data into train and test data
+        train_data = df[i]
+        target_data = df_flag_sorted['flag_H']
+
+        # .values returns numpy.ndarray
+        # using array instead of dataframe to train model because
+        # array is faster to compute instead of dataframe
+        X = train_data.values
+        y = target_data.values
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = validation_size, random_state = seed)
 
 
-    ## Create a Validation dataset
-    # Define parameters for each model
-    validation_size = 0.30
-    seed = 1234
-    num_splits = 10
-    scoring = 'f1'
+        ## Evaluate Algorithms: Baseline
+        # Spot-check Algorithms
+        models = []
 
-    # split data into train and test data
-    train_data = df_features
-    target_data = df_flag_sorted['flag_H']
+        # In LogisticRegression set: solver='lbfgs',multi_class ='auto', max_iter=10000 to overcome warning
+        models.append(('LR',LogisticRegression(solver='lbfgs',multi_class='auto',max_iter=10000)))
+        models.append(('LDA',LinearDiscriminantAnalysis()))
+        models.append(('KNN',KNeighborsClassifier()))
+        models.append(('CART',DecisionTreeClassifier()))
+        models.append(('NB',GaussianNB()))
+        models.append(('SVM',SVC(gamma='scale')))
+        models.append(('RF', RandomForestClassifier()))
 
-    # .values returns numpy.ndarray
-    # using array instead of dataframe to train model because
-    # array is faster to compute instead of dataframe
-    X = train_data.values
-    y = target_data.values
+        for scoring in scorings:
+            # evaluate each model in turn
+            results = []
+            names = []
+            print('\n'+ scoring)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = validation_size, random_state = seed)
+            with open (f'{file_name}_{scoring}_algorithm_comparison.txt',mode='a') as f:
+                print('\n'+ scoring, file=f)
 
+            # evaluate each model in turn
+            for name, model in models:
 
-    ## Evaluate Algorithms: Baseline
-    # Spot-check Algorithms
-    models = []
+                # StratifiedKFoldの設定
+                # initializing kfold by n_splits=10(no.of K)
+                skf = StratifiedKFold(n_splits = num_splits, shuffle = True, random_state = seed)
 
-    # In LogisticRegression set: solver='lbfgs',multi_class ='auto', max_iter=10000 to overcome warning
-    models.append(('LR',LogisticRegression(solver='lbfgs',multi_class='auto',max_iter=10000)))
-    models.append(('LDA',LinearDiscriminantAnalysis()))
-    models.append(('KNN',KNeighborsClassifier()))
-    models.append(('CART',DecisionTreeClassifier()))
-    models.append(('NB',GaussianNB()))
-    models.append(('SVM',SVC(gamma='scale')))
-    models.append(('RF', RandomForestClassifier()))
+                cv_results = cross_val_score(model, X_train, y_train, cv=skf, scoring=scoring )
+                results.append(cv_results)
+                names.append(name)
 
-    # evaluate each model in turn
-    results = []
-    names = []
+                msg = "{}:{:.3f}({:.3f})".format(name, cv_results.mean(), cv_results.std() )
+                print(msg)
 
-    # evaluate each model in turn
-    for name, model in models:
+                with open (f'{file_name}_{scoring}_algorithm_comparison.txt',mode='a') as f:
+                    print(msg, file=f)
+                    print(str(cv_results.tolist()), file=f)
+                    print('\n', file=f)
+                    # f.write(msg)
+                    # f.writelines(str(cv_results.tolist()))
 
-        # StratifiedKFoldの設定
-        # initializing kfold by n_splits=10(no.of K)
-        skf = StratifiedKFold(n_splits = num_splits, shuffle = True, random_state = seed)
-
-        cv_results = cross_val_score(model, X_train, y_train, cv=skf, scoring=scoring )
-        results.append(cv_results)
-        names.append(name)
-
-        msg = "{}:{:.3f}({:.3f})".format(name, cv_results.mean(), cv_results.std() )
-        print(msg)
-
-        with open ('results_algorithm_comparison.txt',mode='a') as f:
-            print(msg, file=f)
-            print(str(cv_results.tolist()), file=f)
-            print('\n', file=f)
-            # f.write(msg)
-            # f.writelines(str(cv_results.tolist()))
-
-    # Compare Algorithms
-    fig = plt.figure()
-    fig.suptitle('Algorithm Comparison')
-    ax = fig.add_subplot(111)
-    plt.boxplot(results)
-    ax.set_xticklabels(names)
-    plt.show()
-    fig.savefig("Algorithm_Comparison.png")
-
-    ## Evaluate Algorithms: Standardize Data
-    # Define pipeline for each base model
-
-    pipelines = []
-    pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()),('LR', LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=10000))])))
-    pipelines.append(('ScaledLDA', Pipeline([('Scaler', StandardScaler()),('LDA', LinearDiscriminantAnalysis())])))
-    pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN', KNeighborsClassifier())])))
-    pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART', DecisionTreeClassifier())])))
-    pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()), ('NB', GaussianNB())])))
-    pipelines.append(('ScaledSVM', Pipeline([('Scaler', StandardScaler()),('SVM', SVC(gamma='scale'))])))
-    pipelines.append(('ScaledRF', Pipeline([('Scaler', StandardScaler()),('RF',RandomForestClassifier())])))
-
-    # evaluate each model in turn
-    scaled_results = []
-    scaled_names = []
-
-    # evaluate each model in turn
-    for name, model in pipelines:
-
-        # StratifiedKFoldの設定
-        # initializing kfold by n_splits=10(no.of K)
-        skf = StratifiedKFold(n_splits = num_splits, shuffle = True, random_state = seed)
-
-        cv_results = cross_val_score(model, X_train, y_train, cv=skf, scoring=scoring )
-        scaled_results.append(cv_results)
-        scaled_names.append(name)
-
-        msg = "{}:{:.3f}({:.3f})".format(name, cv_results.mean(), cv_results.std() )
-        print(msg)
-        with open ('results_scaled_algorithm_comparison.txt',mode='a') as f:
-            print(msg, file=f)
-            print(str(cv_results.tolist()), file=f)
-            print('\n', file=f)
-            # f.write(msg)
-            # f.writelines(str(cv_results.tolist()))
+            # Compare Algorithms
+            fig = plt.figure()
+            fig.suptitle(f'{file_name} {scoring} Algorithm Comparison')
+            ax = fig.add_subplot(111)
+            plt.boxplot(results)
+            ax.set_xticklabels(names)
+            plt.show()
+            fig.savefig(f"{file_name}_{scoring}_Algorithm_Comparison.png")
 
 
-    # Compare Algorithms
-    fig = plt.figure()
-    fig.suptitle('Algorithm Comparison (Scaled)')
-    ax = fig.add_subplot(111)
-    plt.boxplot(scaled_results)
-    ax.set_xticklabels(scaled_names)
-    plt.show()
-    fig.savefig("Algorithm_Comparison_Scaled.png")
+        # if the numbers of features are less than CVEs with flag, recover the dropped CVEs with flag
+        if (file_name == '02_retweet_count.csv'):
+            df_flag_sorted = df_flag_temp
+
+    # ## Evaluate Algorithms: Standardize Data
+    # # Define pipeline for each base model
+
+    # pipelines = []
+    # pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()),('LR', LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=10000))])))
+    # pipelines.append(('ScaledLDA', Pipeline([('Scaler', StandardScaler()),('LDA', LinearDiscriminantAnalysis())])))
+    # pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN', KNeighborsClassifier())])))
+    # pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART', DecisionTreeClassifier())])))
+    # pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()), ('NB', GaussianNB())])))
+    # pipelines.append(('ScaledSVM', Pipeline([('Scaler', StandardScaler()),('SVM', SVC(gamma='scale'))])))
+    # pipelines.append(('ScaledRF', Pipeline([('Scaler', StandardScaler()),('RF',RandomForestClassifier())])))
+
+    # # evaluate each model in turn
+    # scaled_results = []
+    # scaled_names = []
+
+    # # evaluate each model in turn
+    # for name, model in pipelines:
+
+    #     # StratifiedKFoldの設定
+    #     # initializing kfold by n_splits=10(no.of K)
+    #     skf = StratifiedKFold(n_splits = num_splits, shuffle = True, random_state = seed)
+
+    #     cv_results = cross_val_score(model, X_train, y_train, cv=skf, scoring=scoring )
+    #     scaled_results.append(cv_results)
+    #     scaled_names.append(name)
+
+    #     msg = "{}:{:.3f}({:.3f})".format(name, cv_results.mean(), cv_results.std() )
+    #     print(msg)
+    #     with open ('results_scaled_algorithm_comparison.txt',mode='a') as f:
+    #         print(msg, file=f)
+    #         print(str(cv_results.tolist()), file=f)
+    #         print('\n', file=f)
+    #         # f.write(msg)
+    #         # f.writelines(str(cv_results.tolist()))
+
+
+    # # Compare Algorithms
+    # fig = plt.figure()
+    # fig.suptitle('Algorithm Comparison (Scaled)')
+    # ax = fig.add_subplot(111)
+    # plt.boxplot(scaled_results)
+    # ax.set_xticklabels(scaled_names)
+    # plt.show()
+    # fig.savefig("Algorithm_Comparison_Scaled.png")
 
 
 if __name__ == "__main__":
